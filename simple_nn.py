0f02e9a76753a91298274b8b3d232e60042dd80b
Beondel <bengmacmillan@gmail.com>
2017-06-26 01:26:55 -0400

    first commit

---
+++ simple_nn.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+import input_data
+
+# variables
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+W = tf.Variable(tf.zeros([784, 10]))
+b = tf.Variable(tf.zeros([10]))
+init = tf.global_variables_initializer()
+
+# model
+Y = tf.nn.softmax(tf.matmul(tf.reshape(X, [-1, 784]), W) + b)
+
+# placeholder for correct answers
+Y_ = tf.placeholder(tf.float32, [None, 10])
+
+# loss function
+cross_entropy = -tf.reduce_sum(Y_ * tf.log(Y))
+
+# percent of correct answers found in batch
+is_correct = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(is_correct, tf.float32))
+
+# training step
+optimizer = tf.train.GradientDescentOptimizer(0.003)
+train_step = optimizer.minimize(cross_entropy)
+
+sess = tf.Session()
+sess.run(init)
+
+for i in range(10000):
+    # load batch of images and correct answers
+    batch_X, batch_Y = mnist.train.next_batch(100)
+    train_data = {X: batch_X, Y_: batch_Y}
+
+    # train
+    sess.run(train_step, feed_dict=train_data)
+
+    # success?
+    test_data = {X: mnist.test.images, Y_: mnist.test.labels}
+    a, c = sess.run([accuracy, cross_entropy], feed_dict=test_data)
