204c2749008cc94e86089847e1af22c74cea3487
Beondel <bengmacmillan@gmail.com>
2017-07-03 10:13:37 -0400

    test

--- nn_3.py
+++ nn_3.py
@@ -14,17 +14,17 @@ y = tf.placeholder(tf.float32)
 
 
 def deep_nn(input):
-    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([28 * 28, hl1_nodes], mean=0.0, stddev=0.5)),
-                      'biases': tf.Variable(tf.random_normal([hl1_nodes], mean=0.0, stddev=0.5))}
+    hidden_layer_1 = {'weights': tf.Variable(tf.random_normal([28 * 28, hl1_nodes], mean=0.0, stddev=0.1)),
+                      'biases': tf.Variable(tf.random_normal([hl1_nodes], mean=0.0, stddev=0.1))}
 
-    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([hl1_nodes, hl2_nodes], mean=0.0, stddev=0.5)),
-                      'biases': tf.Variable(tf.random_normal([hl2_nodes], mean=0.0, stddev=0.5))}
+    hidden_layer_2 = {'weights': tf.Variable(tf.random_normal([hl1_nodes, hl2_nodes], mean=0.0, stddev=0.1)),
+                      'biases': tf.Variable(tf.random_normal([hl2_nodes], mean=0.0, stddev=0.1))}
 
-    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([hl2_nodes, hl3_nodes], mean=0.0, stddev=0.5)),
-                      'biases': tf.Variable(tf.random_normal([hl3_nodes], mean=0.0, stddev=0.5))}
+    hidden_layer_3 = {'weights': tf.Variable(tf.random_normal([hl2_nodes, hl3_nodes], mean=0.0, stddev=0.1)),
+                      'biases': tf.Variable(tf.random_normal([hl3_nodes], mean=0.0, stddev=0.1))}
 
-    output_layer = {'weights': tf.Variable(tf.random_normal([hl3_nodes, n_classes], mean=0.0, stddev=0.5)),
-                    'biases': tf.Variable(tf.random_normal([n_classes], mean=0.0, stddev=0.5))}
+    output_layer = {'weights': tf.Variable(tf.random_normal([hl3_nodes, n_classes], mean=0.0, stddev=0.1)),
+                    'biases': tf.Variable(tf.random_normal([n_classes], mean=0.0, stddev=0.1))}
 
     l1 = tf.nn.relu_layer(input, hidden_layer_1['weights'], hidden_layer_1['biases'])
     l2 = tf.nn.relu_layer(l1, hidden_layer_2['weights'], hidden_layer_2['biases'])
@@ -36,7 +36,7 @@ def train(x):
     prediction = deep_nn(x)
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(cost)
-    n_epochs = 10
+    n_epochs = 15
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
@@ -47,7 +47,7 @@ def train(x):
                 mnist_x, mnist_y = mnist.train.next_batch(batch_size)
                 i, c = sess.run([optimizer, cost], feed_dict={x: mnist_x, y: mnist_y})
                 epoch_loss += c
-            print('Epoch', epoch, 'completed out of', n_epochs, 'loss:', epoch_loss)
+            print('Epoch', epoch, 'completed out of', n_epochs - 1, 'loss:', epoch_loss)
 
         correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
         accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
